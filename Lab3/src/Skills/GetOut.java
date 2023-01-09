dd8b98b35fd9316d1974200609b5f31b0565624e
GrigoryWolf <122224109+GrigoryWolf@users.noreply.github.com>
2023-01-09 10:52:12 +0300

    Update GetOut.java

--- Lab3/src/Skills/GetOut.java
+++ Lab3/src/Skills/GetOut.java
@@ -2,7 +2,7 @@ package Skills;
 
 import Filled.Vehicle;
 
-public class GetOut implements Skill {
+public class GetOut{
     public static void skill(Vehicle vehicle){
         System.out.println("Из транспорта " + vehicle.getName() + " " + vehicle.getColor() + " " + vehicle.getType() + " выходит(-ят) " + vehicle.getPassanges());
     }
