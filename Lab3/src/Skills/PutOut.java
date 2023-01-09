9620f7d74337d6ae3fb0fc7698b8e5438e47fbbd
GrigoryWolf <122224109+GrigoryWolf@users.noreply.github.com>
2023-01-09 10:51:16 +0300

    Update PutOut.java

--- Lab3/src/Skills/PutOut.java
+++ Lab3/src/Skills/PutOut.java
@@ -1,7 +1,7 @@
 package Skills;
 import Filled.Filled;
 import Person.Person;
-public class PutOut implements Skill{
+public class PutOut{
     public static void skill(Person person, Filled filled){
         System.out.println(person.getName() + " достаёт из " + filled.getName() + " " + filled.getFilling());
     }
